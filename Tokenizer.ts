b6f658e5abb92e719a7af459df0c7dcf17b77a72
Matthew Klein <47091781+mklein97@users.noreply.github.com>
2020-01-26 17:29:40 -0500

    updated tokenizer.ts

--- Tokenizer.ts
+++ Tokenizer.ts
@@ -18,8 +18,29 @@ export class Tokenizer
     }
     next(): Token 
     {
-        ...return next token...
-        ...advance this.idx...
-        ...adjust this.currentLine...
+        if( this.idx >= this.inputData.length ){
+            //special "end of file" metatoken
+            return new Token("$",undefined,this.lineNumber);
+        }
+        
+        for(let i=0;i<this.grammar.terminals.length;++i){
+            let terminal = this.grammar.terminals[i];
+            let sym = terminal.sym;
+            let rex = terminal.rex;     //RegExp
+            rex.lastIndex = this.idx;   //tell where to start searching
+            let m = rex.exec(this.inputData);   //do the search
+            if( m ){
+                //m[0] contains matched text as string
+                let lexeme = m[0];
+                if( sym !== "WHITESPACE" && sym !== "COMMENT" ){
+                    //return new Token using sym, lexeme, and line number
+                } else {
+                    //skip whitespace and get next real token
+                    return this.next();
+                }
+            }
+        }
+        //no match; syntax error
+        throw new Error(...);
     }
 }
